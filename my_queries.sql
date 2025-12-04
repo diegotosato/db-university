075d0c63265c7331fc79f1e105f1b71e1fb79826
diegotosato <tosato1998@gmail.com>
2025-12-04 18:23:05 +0100

    Uncomment SQL query to select courses with more than 10 credits and improve formatting

--- my_queries.sql
+++ my_queries.sql
@@ -6,7 +6,15 @@ WHERE YEAR(`date_of_birth`) = 1990
 */
 
 # Selezionare tutti i corsi che valgono più di 10 crediti (479)
+/*
 SELECT *
 FROM `courses`
 WHERE `cfu` > 10
 ORDER BY `cfu`
+*/
+
+# Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT *
+FROM `students`
+WHERE YEAR(`date_of_birth`) > 1995
+ORDER BY YEAR(`date_of_birth`)
